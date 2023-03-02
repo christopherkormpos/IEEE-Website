b6e5f492ce18657699dbbfeb86e73f071bd1df93
unknown <christopher.kormpos@gmail.com>
2023-03-02 15:23:19 +0200

    added news from api

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios';
 
 export default function News(){
+    const [data, setData] = React.useState([])      //State for incoming API data
+
+
+    React.useEffect(() => {
+        axios.get('http://localhost:8000').then((response) => {     //useEffect hook for setting our API data
+            setData(response.data)
+        })
+    })
+
     return (
-        <div>
-            NEWS
-        </div>
+        data.length > 0 ?               //Ternary operator
+        <div className="wrapper">      
+                {data.map((card) => {
+                    return (
+                        <div>
+                            <h4>{card.newsTitle}</h4>
+                            <p>{card.newsUrl}</p>
+                            <p>{card.newsTimetoRead}</p>
+                            <p>{card.newsDatePublished}</p>
+                            <p>{card.newsSection}</p>
+                            <img src={card.newsImgSrc} alt="" />
+                        </div>
+                    )
+                })}
+            </div>
+
+            : <div>ERROR OCCUERD</div>
+
+
     )
+
   }
