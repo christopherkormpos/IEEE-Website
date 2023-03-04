78dfd0f93872e1c1f50b1556770ce525f7e3a747
vagg <vagg@example.com>
2023-03-04 11:49:57 +0200

    added data structures

---
+++ src/data/projects.js
@@ -0,0 +1,99 @@
+const data = [
+  {
+    id: 1,
+    title: "Dataset creation for chatbot training",
+    text:
+      " Creation of a dataset for intent classification and Named-Entity Recognition (NER) for training a BERT-based model." +
+      " It will be used for answering student or interested parties questions about the educational and research activities" +
+      " of a research lab and student clubs of a University. The dataset will be used in order to prepare a prototype chatbot" +
+      " for the CONCERT lab and the University of West Attica IEEE Student branch.",
+
+    image: "../images/projects/chatbot.png",
+  },
+  {
+    id: 2,
+    title:
+      "Development of a 'digital wardrobe' with the ability to suggest clothing",
+    text:
+      " Creation of a digital wardrobe in which we can register metadata (descriptions) or photos of the objects it will contain." +
+      " Items will be stored in our digital wardrobe with a lot of detail but be distinct enough so that the app can display them" +
+      " with a small range of search options. For the specific project, there is a specific usage scenario like selection of clothing" +
+      " related to events, etc.",
+    image: "../images/projects/wardrobe.png",
+  },
+  {
+    id: 3,
+    title:
+      "Status survey and description of an animal activity data recording framework",
+    text:
+      " In the context of the specific project, the literature and the existing situation regarding devices and platforms" +
+      " for monitoring animal activity will be studied, while a platform will be designed that will use existing devices" +
+      " (WAMPA, Digitanimal, web cams) to create a dataset with animal activity , for use in Deep Learning models.",
+    image: "../images/nia.png",
+  },
+  {
+    id: 4,
+    title:
+      "Installation and setup of a social networking platform for university students",
+    text:
+      " Use of the MASTODON open source social network platform, to create a social network only for students of a specific department" +
+      " with the aim of serving their communication needs, free from advertisements or messages from users unrelated to the University.",
+    image: "../images/projects/mastodon.png",
+  },
+  {
+    id: 5,
+    title: "Designing a University introduction game using Scavify software",
+    text:
+      " Creation of game scenarios through the Scavify app to get to know the University, its spaces and the services. It is intended" +
+      " to be used by new students or Erasmus students.The games will be played on mobile, and will take the form of a treasure hunt.",
+    image: "../images/nia.png",
+  },
+  {
+    id: 6,
+    title: "Creating a 360° rescuer training presentation application",
+    text:
+      " Creation of web pages or a mobile application through which you can present a rescue operation in a tunnel in the form of 360 videos," +
+      " using already available material.",
+    image: "../images/rescuer_presentation.png",
+  },
+  {
+    id: 7,
+    title:
+      "Creation of comic strips on the topic of rescuer support technologies",
+    text:
+      " Use of a special application (e.g. on ipad) to convert photos and presentations into comic strips that will present the results and" +
+      " activities of the RESCUER research project.",
+    image: "../images/nia.png",
+  },
+  {
+    id: 8,
+    title: "Design a demonstration of UWB radar usage in rescue operations",
+    text:
+      " Design of a UWB radar demonstration scenario for use in rescue operations, with the aim of presenting it at a technology fair." +
+      " Students participating in this project will be the ones presenting it at the fair.",
+    image: "../images/radar.png",
+  },
+  {
+    id: 9,
+    title:
+      "Interactive presentation of the University and its premises using 360 video and images",
+    text: " Use of 360 camera to create an interactive presentation of the University on a website (or mobile application).",
+    image: "../images/nia.png",
+  },
+  {
+    id: 10,
+    title: "Design and 3D printing parts of a service robot",
+    text:
+      " Design and print the body of a service robot (with the possibility of autonomous movement, communication with users)," +
+      " which will combine existing hardware implementations in the CONSENT laboratory.",
+    image: "../images/nia.png",
+  },
+  {
+    id: 11,
+    title: "Student Branch website creation",
+    text: " Ongoing work on the current website for the ieee Studen Branch of UniWA.",
+    image: "../images/nia.png",
+  },
+];
+
+export default data;
---
+++ src/data/sbnews.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    id: 1,
+    date: "30th Sept, 2022",
+    title: "Researcher’s Night 2022 of NTUA",
+    text:
+      " The volunteers of UNIWA’s IEEE Student Branch Phoeve Bertsia, Dimitra Motsakou, Nikos Plakas, Henry Gindi, Dimitris Tzagarakis," +
+      " Christos Tsillis and Thomas Doupis have participated in the presentation of CoNSeRT Lab’s projects, in Researcher’s Night 2022 of NTUA.",
+    image: "../images/news/researchers_night_2022.png",
+  },
+  {
+    id: 2,
+    date: "8th-9th Oct, 2022",
+    title: "IEEE Greece Section - Lead your Student Branch",
+    text: "We participated in IEEE Greece Section- Lead your Student Branch, Lamia 2022, as well! Phoeve Bertsia, Eleni Moniaki",
+    image: "../images/news/lamia2022.png",
+  },
+  {
+    id: 3,
+    date: "21st-23nd Oct, 2022",
+    title: "Athens Science Festival 2022",
+    text:
+      " Phoeve Bertsia, Eleni Moniaki and Eleni Agrianiti, volunteers of IEEE UniWA Student Branch, participated in the presentation of IEEE UniWA SB’s projects," +
+      " in 2022 Athens Science Festival.",
+    image: "../images/news/athens_sci_fest_2022_1",
+  },
+  {
+    id: 4,
+    date: "22nd Oct, 2022",
+    title: "IEEEXTREME Programming Competition 16.0",
+    text:
+      " Congratulations to Dimitra Motsakou, Evangelos Mitikas and Konstantinos Bandelis, who represented team UniWAPegasus after three years of abstention!\n" +
+      " Special thanks to IEEE NTUA SB for their kind hospitality in the National Technical University of Athens!",
+    image: "../images/news/ieeextreme16.png",
+  },
+  {
+    id: 5,
+    date: "4th Dec, 2022",
+    title:
+      "The 1st IEEE Greece Mentoring Program event, for students and graduates!",
+    text: `
+        The 1st IEEE Greece Mentoring Program event, for students and graduates!
+
+        Event organized in cooperation with:
+        •	IEEE Young Professionals Greece
+        •	IEEE UniWA Student Branch & IEEE NTUA Student Branch
+        •	IEEE WIE Greece
+        •	IEEE Greece Computer Chapter, 
+        •	IEEE Greece Section
+        •	IEEE Life members Greece
+
+        The event took place on December 7, 2022, in the Multimedia Laboratory of the NTUA’s Library & Information Center. We have prepared some interesting speeches to help you promote your career and empower your, to inspire you for your next steps or even… encourage you to participate in the rest of our actions.
+
+        Program Contents:
+        •	Introduction to IEEE Greece Mentoring Program
+        •	Mentoring: A relationship which inspires and empowers (Marianna Terzidaki, MSc in Engineering, Career Coach, approved by ICF)
+        •	Live mentoring at the maximum (Ioannis Ipliktsiadis, MSc Electrical Engineer, Technical University of Denmark)
+        •	Stay or leave? The paths after the degree (Kostas Karpouzis, Assistant Professor at the Department of Communication, Media and Culture, Panteion University)
+    `,
+    image: "../images/news/mentoring_program.png",
+  },
+];
+
+export default data;
